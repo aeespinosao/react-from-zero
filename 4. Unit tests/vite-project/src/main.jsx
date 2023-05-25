93f3daaabb5a46c576af6fd9fb6aadba96106d30
Andres Espinosa <andreseespinosao@gmail.com>
2023-05-24 22:41:32 -0500

    vite project with jest

---
+++ 4. Unit tests/vite-project/src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { CounterApp } from "./CounterApp";
+import { FirstApp } from "./FirstApp";
+import { HelloWroldApp } from "./HelloWroldApp";
+
+import "./styles.css";
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <CounterApp value={21}/>
+    </React.StrictMode>
+)
