d5754a9d746012cf1f84e3edb8e1fb13cae645cc
Andres Espinosa <andreseespinosao@gmail.com>
2023-06-22 00:23:06 -0500

    request data from api

--- 6. Gif Expert App/src/GifExpertApp.jsx
+++ 6. Gif Expert App/src/GifExpertApp.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
 
     const [categories, setCategories] = useState(["test"]);
 
     const onAddCategory = (newCategory) => {
+        if( categories.includes(newCategory) ) return;
+
         setCategories([newCategory, ...categories])
     }
 
@@ -14,11 +17,10 @@ export const GifExpertApp = () => {
             <h1>GifExpertApp</h1>
 
             <AddCategory onNewCategory = { onAddCategory } />
-            <ol>
-                { 
-                    categories.map(category => { return <li key={ category }>{category}</li> }) 
-                }
-            </ol>
+            
+            { 
+                categories.map(category => (<GifGrid key={category} category={category}/>)) 
+            }
         </>
     )
 }
